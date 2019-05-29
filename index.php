1bec04d1c70a2cf27dba63946b1bd0700245e02f
Chivv <51078869+Chivv@users.noreply.github.com>
2019-05-29 20:18:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,7 @@
 <body>
 <a href ="create_form.php">Toevoegen</a>
 <?php
-$host = 'localhost';
-$dbname = 'FCA';
-$username = 'root';
-$password = 'Root';
-
-$connectStr = 'mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8';
-$db = new PDO($connectStr, $username, $password);
-
-$sql = "SELECT * FROM Customer";
-$sth = $db->prepare($sql);
-$sth->execute();
+include "script/connectdb.php";
 ?>
 
 <table class = "table">
