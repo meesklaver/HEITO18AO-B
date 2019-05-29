e2bc5d3b58446524e33af012e993c6ee14e8a761
Chivv <51078869+Chivv@users.noreply.github.com>
2019-05-29 20:30:06 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <body>
+    <div class="d-flex" id="wrapper">
+
+    <!-- Sidebar -->
+    <div class="bg-light border-right" id="sidebar-wrapper">
+        <div class="sidebar-heading">Start Bootstrap </div>
+        <div class="list-group list-group-flush">
+            <a href="#" class="list-group-item list-group-item-action bg-light">Dashboard</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Shortcuts</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Profile</a>
+            <a href="#" class="list-group-item list-group-item-action bg-light">Status</a>
+        </div>
+    </div>
+
 <a href ="create_form.php">Toevoegen</a>
 <?php
 include "script/connectdb.php";
